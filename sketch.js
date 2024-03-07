fc273d87bab155b6e6312a6fa25d342e6757f73a
Schnittk4 <153139206+Schnittk4@users.noreply.github.com>
2024-03-06 18:21:02 -0800

    chaoticPrtkls

--- sketch.js
+++ sketch.js
@@ -1,26 +1,30 @@
 let velocity;
 let gravity;
-let counterForce;
+let uCounterForce;
 let rCounterForce;
-let lCounterForce;
-let theta;
-let inertia;
+let lrCounterForce;
 let parties = [];
+let dark = true;
 // make the counterForce direction equal  PI - 2 * theta  
 function setup() {
   createCanvas(800, 800);
-  for(let i = 0; i < 20; i++){
+  background(0, 0, 40);
+  for(let i = 0; i < 1000; i++){
     parties[i] = new Part();
   }
   // inertia = createVector(random(-0.05, 0.05), 0);
   gravity = createVector(0, 0.2);
-  counterForce = createVector(0, -0.5);
+  lrCounterForce = createVector(0, -0.5);
+  lCounterForce = createVector(0.2, 0);
+  rCounterForce = createVector(-0.2, 0);
+  uCounterForce = createVector(0, 0.15)
+
 }
 
 function draw() {
-  background(225);
+  // background(0, 0, 40);
 
-  for(let i = 0; i < 20; i++){
+  for(let i = 0; i < 1000; i++){
     // parties[i] = new Part();
     parties[i].display();
     parties[i].move();
@@ -31,22 +35,44 @@ function draw() {
 class Part{
   constructor(){
   this.location = createVector(random(width), 0);
-  this.velocity = createVector(0, random(5));
+  this.velocity = createVector(0, random(10));
 
   }
   move(){
 
     this.location.add(this.velocity);
     this.velocity.add(gravity);
-    if(this.location.y > height/2){
-      // this.location.add(velocity);
-      this.velocity.add(counterForce);
-      
-      // this.counterForce = rotate(PI - 2 * theta);   
+    if(this.location.y > height/2){     
+      this.velocity.add(lrCounterForce);       
+    }
+    if(this.location.y < height / 3){
+      this.velocity.add(uCounterForce);
+    }
+    if(this.location.x < width/2){
+      this.velocity.add(lCounterForce);
+    }
+    if(this.location.x > width/2){
+      this.velocity.add(rCounterForce);
     }
   }
   display(){
-    fill(0);
-    ellipse(this.location.x, this.location.y, 5, 5);
+    noStroke();
+    if(dark){
+      fill(255);
+      ellipse(this.location.x, this.location.y, random(1, 3), random(1, 3));
+    }else{
+      fill(0, 0, 40);
+      ellipse(this.location.x, this.location.y, random(1, 3), random(1, 3))
+    }
   }
+}
+
+function keyPressed(){
+if(keyCode === 32 && dark == true){
+  dark = false;
+}else{
+  dark = true;
+}
+
+
 }
