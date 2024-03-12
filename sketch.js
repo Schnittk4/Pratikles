01d917c89ab269c116a5054d811d33817e4bfda8
Schnittk4 <153139206+Schnittk4@users.noreply.github.com>
2024-03-12 11:14:10 -0700

    added mousevity

--- sketch.js
+++ sketch.js
@@ -3,13 +3,18 @@ let gravity;
 let uCounterForce;
 let rCounterForce;
 let lrCounterForce;
+let clickForce;
+let clickForceR;
+let clickForceU;
+let clickForceD;
 let parties = [];
 let dark = true;
+let partAmount = 1000;
 // make the counterForce direction equal  PI - 2 * theta  
 function setup() {
   createCanvas(800, 800);
   background(0, 0, 40);
-  for(let i = 0; i < 1000; i++){
+  for(let i = 0; i < partAmount; i++){
     parties[i] = new Part();
   }
   // inertia = createVector(random(-0.05, 0.05), 0);
@@ -17,14 +22,14 @@ function setup() {
   lrCounterForce = createVector(0, -0.5);
   lCounterForce = createVector(0.2, 0);
   rCounterForce = createVector(-0.2, 0);
-  uCounterForce = createVector(0, 0.15)
+  uCounterForce = createVector(0, 0.15);
 
 }
 
 function draw() {
-  // background(0, 0, 40);
+  background(0, 0, 40);
 
-  for(let i = 0; i < 1000; i++){
+  for(let i = 0; i < partAmount; i++){
     // parties[i] = new Part();
     parties[i].display();
     parties[i].move();
@@ -67,12 +72,34 @@ class Part{
   }
 }
 
+function pushPull(particle){  
+  // let pull = clL--;
+  let mouseMass = 100;
+  // clickForceR = createVector(clR, 0);
+  // clickForceU = createVector(0, -clU);
+  // clickForceD = createVector(0, clD);
+  let mousevity = createVector(mouseX, mouseY);
+  mousevity.mult(mouseMass);
+  for(i = 0; i < partAmount; i++){    
+    mousevity.add(particle.location);
+    particle.location = particle.location.add(mousevity);
+  }
+  // clickForceR.mult(loc);
+  // clickForceD.mult(loc);
+  // clickForceU.mult(loc);
+  
+}
+
 function keyPressed(){
-if(keyCode === 32 && dark == true){
-  dark = false;
-}else{
-  dark = true;
+  if(keyCode === 32 && dark == true){
+    dark = false;
+  }else{
+    dark = true;
+  }
 }
 
-
+function mouseDragged(){
+  for(i = 0; i < partAmount; i++)
+    pushPull(parties[i]);
 }
+
